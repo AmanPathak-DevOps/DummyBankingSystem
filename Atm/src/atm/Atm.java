70a25b3970536b3915621b9befd6ca5dc599788f
Aman Pathak <aman07pathak@gmail.com>
2021-10-03 11:15:23 +0530

    first commit

---
+++ Atm/src/atm/Atm.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+/**
+ *
+ * @author Aman
+ */
+public class Atm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Open op = new Open();
+       op.setVisible(true);
+    }
+    
+}
